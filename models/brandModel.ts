5b88ae1b71bbf26491217cef5cf16b7fd3aaa64b
nikhilBornare <nikhilbornare01@gmail.com>
2024-12-12 15:15:52 +0530

    defined brandModel Schema

---
+++ models/brandModel.ts
@@ -0,0 +1,71 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+// Define the Brand interface
+export interface IBrand extends Document {
+    name: string;
+    description: string;
+    website: string;
+    email: string;
+    country: string;
+    foundedYear: number;
+    status: string;
+    availableLocation: string;
+    totalProduct: number;
+    rating: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+// Define the Brand schema
+const brandSchema: Schema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        website: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        country: {
+            type: String,
+            required: true
+        },
+        foundedYear: {
+            type: Number,
+            required: true
+        },
+        status: {
+            type: String,
+            required: true, enum: ["Active", "Inactive"]
+        },
+        availableLocation: {
+            type: String,
+            required: true
+        },
+        totalProduct: {
+            type: Number,
+            required: true
+        },
+        rating: {
+            type: Number,
+            required: true,
+            min: 0,
+            max: 5
+        },
+    },
+    {
+        timestamps: true, // Automatically adds createdAt and updatedAt fields
+    }
+);
+
+export default mongoose.model<IBrand>("Brand", brandSchema);
